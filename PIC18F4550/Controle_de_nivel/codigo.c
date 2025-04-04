55aadd0a46f29ab6c7bba7f4f07f5da1f145f9fe
Gustavo Lima <eng.lima1911@gmail.com>
2025-04-04 09:12:49 -0300

    Controle de nivel
    Código do projeto de controle de nível de um reservatório

--- PIC18F4550/Controle_de_nivel/codigo.c
+++ PIC18F4550/Controle_de_nivel/codigo.c
@@ -1,25 +1,44 @@
-// ISSO É SÓ UM TESTE DE CONFIGURAÇÃO
+#include <pic18f4550.h>
 
-#include <xc.h>
+#define BT_on PORTAbits.RA0
+#define BT_off PORTAbits.RA1
+#define S1_min PORTBbits.RB0
+#define S2_max PORTBbits.RB1
+#define LED PORTCbits.RC0
+#define motor PORTCbits.RC1
 
-// Configuração dos bits de configuração do PIC18F4550
-#pragma config FOSC = HSPLL_HS // Oscilador em alta velocidade
-#pragma config PLLDIV = 5      // Divisor do PLL
-#pragma config CPUDIV = OSC1_PLL2
-#pragma config VREGEN = ON     // Regulador de tensão USB ativado
-#pragma config WDT = OFF       // Watchdog Timer desligado
-#pragma config PBADEN = OFF    // Port B configurado como digital
-#pragma config LVP = OFF       // Desabilita programação em baixa voltagem
+void main(){
 
-#define _XTAL_FREQ 48000000  // Define a frequência do oscilador (48MHz)
+    //Ajuste da frequencia interna do microcontrolador para 2Mhz com uma fonte de clock interna
+    OSCCON = 0x5E;
 
-void main(void) {
-    TRISBbits.TRISB0 = 0; // Configura RB0 como saída
+    //Ajuste dos pinos de saída e entrada
+    TRISA = 0x03; //Pinos a são input
+    TRISB = 0x03; //Pinos b são input
+    TRISC = 0x00; //Pinos c são output
 
-    while (1) {
-        LATBbits.LATB0 = 1; // Liga o LED
-        __delay_ms(500);    // Espera 500ms
-        LATBbits.LATB0 = 0; // Desliga o LED
-        __delay_ms(500);    // Espera 500ms
+    //Desabilita a função analógica dos pinos ANx
+    ADCON1 = 0x0F;
+
+    //Loop do programa
+    while (1)
+    {
+        //Controle automatico ativado
+        if (BT_on == 1 && BT_off == 0){
+            LED = 1;
+            //verifica se o nivel esta abaixo do minimo e então, caso sim, liga o motor
+            if(S1_min == 0 && S2_max == 0){
+                motor = 1;
+            }
+            //verifica se o nivel esta acima do maximo e então, caso sim, desliga o motor
+            if (S1_min == 1 && S2_max == 1){
+                motor = 0;
+            }
+        }
+        //Controle automatico desativado
+        if (BT_off == 1){
+            motor = 0;
+            LED = 0;
+        }
     }
 }
